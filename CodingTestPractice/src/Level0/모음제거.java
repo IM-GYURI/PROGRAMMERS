5dd0c68e3642942a9ce3b62d8f1e7baa0feba198
IM-GYURI <kgulr0517@gmail.com>
2024-02-27 17:36:48 +0900

    모음 제거

---
+++ CodingTestPractice/src/Level0/모음제거.java
@@ -0,0 +1,44 @@
+package Level0;
+
+import java.util.ArrayList;
+
+class Solution6 {
+    public String solution(String my_string) {
+        ArrayList<Character> list = new ArrayList<>();
+        char[] aeiou = {'a', 'e', 'i', 'o', 'u'};
+        int idx = 0;
+        boolean isOK = false;
+
+        for (char c : my_string.toCharArray()) {
+            for (int i = 0; i < aeiou.length; i++) {
+                if (c != aeiou[i]) {
+                    isOK = true;
+                } else {
+                    isOK = false;
+                    break;
+                }
+            }
+            if (isOK) {
+                list.add(c);
+            }
+        }
+
+        String result = "";
+        for (char c: list) {
+            result += c + "";
+        }
+
+        return result;
+    }
+}
+
+public class 모음제거 {
+    public static void main(String[] args) {
+        Solution6 solution6 = new Solution6();
+        String my_string = "bus";
+        System.out.println(solution6.solution(my_string));
+
+        my_string = "nice to meet you";
+        System.out.println(solution6.solution(my_string));
+    }
+}
