3e1ea71e8b446624e924db3815b85ed9405452ef
IM-GYURI <kgulr0517@gmail.com>
2024-02-28 00:14:45 +0900

    재풀이

--- CodingTestPractice/src/Level1/신규아이디추천.java
+++ CodingTestPractice/src/Level1/신규아이디추천.java
@@ -3,7 +3,7 @@ package Level1;
 import java.util.ArrayList;
 
 class Solution7 {
-    public String solution(String new_id) {
+/*    public String solution(String new_id) {
         ArrayList<Character> list = new ArrayList<>();
         String answer = "";
 
@@ -77,6 +77,30 @@ class Solution7 {
             answer += c;
         }
 
+        return answer;
+    }*/
+
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase();   // 1단계
+
+        answer = answer.replaceAll("[^-_.a-z0-9]", ""); // 2단계
+        answer = answer.replaceAll("[.]{2,}", ".");     // 3단계
+        answer = answer.replaceAll("^[.]|[.]$", "");    // 4단계
+
+        if (answer.equals("")) {    // 5단계
+            answer += "a";
+        }
+
+        if (answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("[.]$", "");
+        }
+
+        if (answer.length() <= 2) {
+            while (answer.length() < 3) {
+                answer += answer.charAt(answer.length() - 1);
+            }
+        }
         return answer;
     }
 }
