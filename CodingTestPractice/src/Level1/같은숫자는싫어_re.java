1fab5771955e70ab911b198b3d9c38a150bf3abb
IM-GYURI <kgulr0517@gmail.com>
2024-03-06 03:16:29 +0900

    같은 숫자는 싫어 - 재풀이

--- CodingTestPractice/src/Level1/같은숫자는싫어_re.java
+++ CodingTestPractice/src/Level1/같은숫자는싫어_re.java
@@ -5,17 +5,20 @@ import java.util.Stack;
 
 class Solution3 {
     public int[] solution(int[] arr) {
-        Stack<Integer> stack = new Stack<>();
+        Stack stack = new Stack();
+        stack.push(arr[0]);
 
-        for (int n : arr) {
-            if (stack.isEmpty() || stack.peek() != n) {
-                stack.add(n);
+        for (int i = 1; i < arr.length; i++) {
+            int num = (int) stack.peek();
+            if (arr[i] != num) {
+                stack.push(arr[i]);
             }
         }
 
         int[] answer = new int[stack.size()];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = stack.get(i);
+
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            answer[i] = (int)stack.pop();
         }
 
         return answer;
