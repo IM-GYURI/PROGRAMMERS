fcbe45dc268992ff32d7207dfb20469db03a1f5c
IM-GYURI <kgulr0517@gmail.com>
2024-03-19 19:18:12 +0900

    하노이의 탑 - 재풀이

--- CodingTestPractice/src/Level2/하노이의탑.java
+++ CodingTestPractice/src/Level2/하노이의탑.java
@@ -7,19 +7,19 @@ class Solution18 {
     List<int[]> arr = new ArrayList<>();
 
     public int[][] solution(int n) {
-        move(n, 1, 2, 3);
+        hanoi(n, 1, 2, 3);
         int[][] answer = arr.stream().toArray(int[][]::new);
         return answer;
     }
 
-    public void move(int cnt, int start, int mid, int end) {
+    public void hanoi(int cnt, int start, int mid, int end) {
         if (cnt == 0) {
             return;
         }
 
-        move(cnt - 1, start, end, mid);
+        hanoi(cnt - 1, start, end, mid);
         arr.add(new int[]{start, end});
-        move(cnt - 1, mid, start, end);
+        hanoi(cnt - 1, mid, start, end);
     }
 }
 
